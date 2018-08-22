65f3028fd6fd615c19dbea182ccb1ea35a40cbf5
Edgar Dewsbery <edewsbery@gmail.com>
2018-08-22 14:21:08 +0100

    integration test

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import CommentList from '../CommentBox.js';
+import CommentList from '../CommentList.js';
 import Root from '../../Root.js'
 
 let wrapped;
 
 beforeEach(() => {
 const initialState = {
-  comments: ['comment1', 'comment2']
+  comments: ['Comment 1', 'Comment 2']
 }
+
   wrapped = mount(
     <Root initialState={initialState}>
       <CommentList />
@@ -17,6 +18,10 @@ const initialState = {
 })
 
 it('creates one <li> per comment', () => {
-  wrapped.update()
-  console.log(wrapped.find('li').length)
+  expect(wrapped.find('li').length).toEqual(2)
+})
+
+it('shows the text for each comment', () => {
+  expect(wrapped.render().text()).toContain('Comment 1')
+  expect(wrapped.render().text()).toContain('Comment 2')
 })
